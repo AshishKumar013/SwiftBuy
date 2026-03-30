244c8933e067513015ad835384a50b7e632e05da
Ashish Kumar <ashish13ip@gmail.com>
2026-03-30 14:04:44 +0530

    fix: made all routes responsive using relative units

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,47 +7,54 @@ import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext)
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { getTotalCartItems } = useContext(ShopContext);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <div className={Classes.navbar}>
       <div className={Classes.nav_logo}>
         <img src={logo} alt="logo" />
         <p>SHOPPER</p>
       </div>
-      <ul className={Classes.nav_menu}>
-        <li
-          onClick={() => {
-            setMenu("shop");
-          }}
-        >
-          <Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu ==="shop"?<hr/>:<></>}
+
+      <ul className={`${Classes.nav_menu} ${menuOpen ? Classes.open : ""}`}>
+        <li onClick={() => { setMenu("shop"); closeMenu(); }}>
+          <Link style={{ textDecoration: "none" }} to="/">Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("mens");
-          }}
-        >
-          <Link style={{textDecoration: 'none'}} to='/mens'>Men</Link> {menu ==="mens"?<hr/>:<></>}
+        <li onClick={() => { setMenu("mens"); closeMenu(); }}>
+          <Link style={{ textDecoration: "none" }} to="/mens">Men</Link>
+          {menu === "mens" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("womens");
-          }}
-        >
-          <Link style={{textDecoration: 'none'}} to='/womens'>Women</Link> {menu ==="womens"?<hr/>:<></>}
+        <li onClick={() => { setMenu("womens"); closeMenu(); }}>
+          <Link style={{ textDecoration: "none" }} to="/womens">Women</Link>
+          {menu === "womens" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("kids");
-          }}
-        >
-          <Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link> {menu ==="kids"?<hr/>:<></>}
+        <li onClick={() => { setMenu("kids"); closeMenu(); }}>
+          <Link style={{ textDecoration: "none" }} to="/kids">Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
+
       <div className={Classes.cart}>
-        <Link to='/login'><button>Login</button></Link>
-        <Link to='/cart'><img src={cart_icon} alt="cart" /></Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="cart" />
+        </Link>
         <div className={Classes.nav_cart_count}>{getTotalCartItems()}</div>
+      </div>
+
+      <div
+        className={Classes.nav_dropdown}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   );
